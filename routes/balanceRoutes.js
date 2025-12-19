aca1d32f0d2392426fe5c8cd899a05390ff0be25
shivanir0210 <shivanirasappan@gmail.com>
2025-12-19 18:26:49 +0530

    Delete routes directory

--- routes/balanceRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { calculateTripBalances } = require('../controllers/balanceController');
-const { protect } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.get('/trip/:tripId', protect, calculateTripBalances);
-
-module.exports = router;
--- routes/tripRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getTrips, createTrip } = require('../controllers/tripController');
-const { protect } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.route('/').get(protect, getTrips).post(protect, createTrip);
-
-module.exports = router;
